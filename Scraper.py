146f5287fe04be5f9b807b53e4746207e31ac268
MichaelLinnihan <mickedylinnihan@gmail.com>
2023-06-13 10:34:12 -0500

    Add files via upload

--- Scraper.py
+++ Scraper.py
@@ -3,7 +3,6 @@ from urllib.request import urlopen
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
-
 def get_sublinks(url):
     page = urlopen(url)
     html_bytes = page.read()
@@ -13,7 +12,7 @@ def get_sublinks(url):
     sublinks = [urljoin(url, anchor['href']) for anchor in anchor_tags if 'href' in anchor.attrs]
     return sublinks
 
-def get_text(url):
+def get_text(url, file_name):
     page = urlopen(url)
     html_bytes = page.read()
     html = html_bytes.decode("utf-8")
@@ -21,10 +20,11 @@ def get_text(url):
     text = html_soup.get_text()
     text = "\n".join(line.strip() for line in text.splitlines() if line.strip())
     wrapped_text = textwrap.fill(text, width=80, initial_indent="    ", subsequent_indent="    ")
-    with open("output.txt", "w", encoding="utf-8") as txt_file:
+    with open(file_name, "w", encoding="utf-8") as txt_file:
         txt_file.write(wrapped_text)
-        
+
 coindesk_url = "https://www.coindesk.com/"
 articles_url = get_sublinks(coindesk_url)
-for article in articles_url:
-    get_text(article)
+for i, article in enumerate(articles_url):
+    file_name = f"output{i+1}.txt"
+    get_text(article, file_name)
